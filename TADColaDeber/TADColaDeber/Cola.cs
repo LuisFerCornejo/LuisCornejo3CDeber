3b54a6cf882b1fd798971d1fcb16cbc261991b2c
LuisFerCornejo <94479281+LuisFerCornejo@users.noreply.github.com>
2021-11-16 15:16:42 -0500

    Add files via upload

---
+++ TADColaDeber/TADColaDeber/Cola.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TADColaDeber
+{
+    class Cola
+    {
+        public Nodo Primero;
+        public Nodo Ultimo;
+
+        public Cola()
+        {
+            Primero = Ultimo = null;
+        }
+
+        public void Encolar (int Valor)
+        {
+            Nodo Aux = new Nodo();
+            Aux.info = Valor;
+            if(Primero == null)
+            {
+                Primero = Ultimo = Aux;
+                Aux.sgte = null;
+            }
+            else
+            {
+                Ultimo.sgte = Aux;
+                Aux.sgte = null;
+                Ultimo = Aux;
+            }
+        
+        }
+        public void Desencolar()
+        {
+            if (Primero == null) Console.WriteLine("Cola Vacía");
+            else Primero = Primero.sgte;
+
+        }
+
+        public int DesencolarValor()
+        {
+            int Valor = 0;
+            if (Primero == null) Console.WriteLine("Cola Vacia");
+            else
+            {
+                Valor = Primero.info;
+                Primero = Primero.sgte;
+            }
+            return Valor;
+
+        }
+
+        public void Mostrar()
+        {
+            if (Primero == null) Console.WriteLine("Cola Vacía");
+            else
+            {
+                Nodo Puntero;
+                Puntero = Primero;
+
+                do
+                {
+                    Console.WriteLine("{0} / t", Puntero.info);
+                    Puntero = Puntero.sgte;
+                }
+                while (Puntero != null);
+            }
+            Console.WriteLine("/n");
+        }
+    }
+}
---
+++ TADColaDeber/TADColaDeber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TADColaDeber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cola objetocola = new Cola();
+            Console.WriteLine("Estableciendo 5 elementos...");
+
+            objetocola.Encolar(4);
+            objetocola.Encolar(10);
+            objetocola.Encolar(2);
+            objetocola.Encolar(54);
+            objetocola.Encolar(12);
+            objetocola.Mostrar();
+
+            Console.WriteLine("Eliminando dos elementos...");
+            objetocola.Desencolar();
+            objetocola.Mostrar();
+            objetocola.Desencolar();
+            objetocola.Mostrar();
+
+            Console.WriteLine("Se va a eliminar un nodo más con el valor de (0)", objetocola.DesencolarValor());
+            objetocola.Mostrar();
+            Console.ReadLine();
+
+        }
+    }
+}
